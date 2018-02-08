8e269d692cb506ff4eb71dbae78c126293cc8e66
jgoch <36202349+jgoch@users.noreply.github.com>
2018-02-07 23:17:59 -0500

    bitch

--- finalProject site/AddPatient.aspx.vb
+++ finalProject site/AddPatient.aspx.vb
@@ -21,10 +21,12 @@ Partial Class _Default
         Catch ex As Exception
 
         End Try
-
+        If Page.IsValid Then
+            lblMessage.Text = "Patient Added"
+        End If
     End Sub
     Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
-
+        lblMessage.Text = ""
     End Sub
 End Class
 
