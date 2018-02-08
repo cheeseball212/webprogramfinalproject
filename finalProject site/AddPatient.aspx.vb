8d1ad4a953fe1a72b0bc02d8a622ebee87680629
cheeseball212 <36201444+cheeseball212@users.noreply.github.com>
2018-02-07 20:54:23 -0500

    Revert "next"
    This reverts commit cc4679e554b734255780f7d8872d344d7ad4d14e.

--- finalProject site/AddPatient.aspx.vb
+++ finalProject site/AddPatient.aspx.vb
@@ -23,9 +23,6 @@ Partial Class _Default
 
         End Try
     End Sub
-    Protected Sub txtDOB_TextChanged(sender As Object, e As EventArgs) Handles txtDOB.TextChanged
-
-    End Sub
 End Class
 
 
